282b3a2211d22702c8af24d6b71df5c712196703
Trent <trent.stenoien@gmail.com>
2018-03-26 16:01:44 -0500

    Final word added & styled hover states

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-// 1. create random board
+// Create random board
 const $board = $('.board');
 const boxes = [];
 $board.children().each(function () {
   boxes.push(...$(this).children());
 });
-const buzzwords = [ 'NEED ONE MORE', 'synergies', 'collaborate', 'shift left', 'ping me', 'low hanging fruit', 'alignment', 'best practice', 'bandwidth', 'transparency', 'leadership', 'piggyback', 'take this offline', 'win-win', 'due diligence', 'drill down', 'manage expectations', 'engagement', 'on/off the radar', 'results driven', 'fail fast', 'incentivize', 'TTUS', 'streamline'];
+const buzzwords = [ 'NEED ONE MORE', 'synergies', 'collaborate', 'shift left', 'ping me', 'low hanging fruit', 'alignment', 'best practice', 'bandwidth', 'transparency', 'leadership', 'piggyback', 'take this offline', 'win-win', 'due diligence', 'drill down', 'manage expectations', 'engagement', 'on/off the radar', 'results driven', 'fail fast', 'incentivize', 'TTUS', 'streamline', 'cloud'];
 boxes.splice(12, 1);
 
 const getRandomBox = () => Math.floor(Math.random() * boxes.length);
@@ -17,6 +17,8 @@ for (let i = 0; i < 24; i++) {
 
 // Listen for clicks and add class
 $('tbody').click(function (e) {
-  if ($(e.target).hasClass('box-filled')) $(e.target).removeClass('box-filled');
-  else $(e.target).addClass('box-filled');
+  if ($(e.target).text() !== 'FREE SPACE') {
+    if ($(e.target).hasClass('box-filled')) $(e.target).removeClass('box-filled');
+    else $(e.target).addClass('box-filled');
+  }
 });
